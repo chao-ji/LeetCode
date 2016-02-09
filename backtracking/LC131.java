8487f087acd78a4eda6e07e48d076c30f489143e
chao-ji <jic@indiana.edu>
2016-02-09 14:36:24 -0500

    Update LC131.java

--- backtracking/LC131.java
+++ backtracking/LC131.java
@@ -3,6 +3,7 @@ public class LC131
 {
 	public static void main(String[] args)
 	{
+		System.out.println(partition("seeslaveidemonstrateyetartsnomedievalsees"));
 	}
 
 	// 131. Palindrome Partitioning
@@ -13,32 +14,44 @@ public class LC131
 		for (int i = 1; i <= len; i++)
 			for (int j = 0; j < len - i + 1; j++)
 				dp[j][j + i - 1] = s.charAt(j) == s.charAt(j + i - 1) && (i <= 2 || dp[j + 1][j + i - 2]);
-		return partition(s, dp, 0, len - 1);
-	}
 
-	static List<List<String>> partition(String s, boolean[][] dp, int l, int h)
-	{
-		List<List<String>> list = new ArrayList<List<String>>();
-		if (dp[l][h])
-		{
-			list.add(new ArrayList<String>());
-			list.get(list.size() - 1).add(s.substring(l, h + 1));
-		}
-		if (l == h)
-			return list;
+		List<List<String>> parts = new ArrayList<List<String>>();
 
-		for (int i = l; i < h; i++)
-			if (dp[l][i])
+		int[] start = new int[1];
+		for (int i = 0; i < dp.length; i++)
+			if (dp[0][i])
 			{
-				List<List<String>> sub = partition(s, dp, i + 1, h);
-				for (int j = 0; j < sub.size(); j++)
-				{
-					List<String> p = new ArrayList<String>();
-					p.add(s.substring(l, i + 1));
-					p.addAll(sub.get(j));
-					list.add(p);
-				}
-			}
-		return list;
+				List<String> part = new ArrayList<String>();
+				part.add(s.substring(0, i + 1));
+				start[0] = i + 1;
+				recursive(parts, part, dp, s, start);
+			}	
+
+		return parts;
 	}
+
+	static void recursive(List<List<String>> parts, List<String> part, boolean[][] dp, String s, int[] start)
+	{
+		if (part.isEmpty())
+			return ;
+		if (start[0] == dp.length)
+		{
+			parts.add(new ArrayList<String>());
+			parts.get(parts.size() - 1).addAll(part);
+
+			start[0] -= part.get(part.size() - 1).length();
+			part.remove(part.size() - 1);
+			return ;
+		}
+
+		for (int i = start[0]; i < dp.length; i++)
+			if (dp[start[0]][i])
+			{
+				part.add(s.substring(start[0], i + 1));
+				start[0] = i + 1;
+				recursive(parts, part, dp, s, start);
+			}
+		start[0] -= part.get(part.size() - 1).length();
+		part.remove(part.size() - 1);		
+	} 
 }
