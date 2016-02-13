2c2c98e613dd198bf988a2ca8b832c2bd57e5ea2
chao-ji <jic@indiana.edu>
2016-02-13 15:38:54 -0500

    Update LC79.java

--- backtracking/LC79.java
+++ backtracking/LC79.java
@@ -11,32 +11,32 @@ public class LC79
 		if (m == 0)
 			return false;
 		int n = board[0].length;
-		boolean[][] dp = new boolean[m][n];
 		for (int i = 0; i < m; i++)
 			for (int j = 0; j < n; j++)
-				if (recursive(board, dp, word, i, j))
+				if (recursive(board, word, i, j))
 					return true;
 		return false;
 	}
 
-	static boolean recursive(char[][] board, boolean[][] dp, String word, int i, int j)
+	static boolean recursive(char[][] board, String word, int i, int j)
 	{
 		if (word.length() == 1 || word.charAt(0) != board[i][j])
 			return word.charAt(0) == board[i][j];
-		dp[i][j] = true;
-		if (j > 0 && dp[i][j - 1] == false)
-			if (recursive(board, dp, word.substring(1), i, j - 1))
+		char hold = board[i][j];
+		board[i][j] = '*';
+		if (j > 0 && board[i][j - 1] != '*')
+			if (recursive(board, word.substring(1), i, j - 1))
 				return true;
-		if (i > 0 && dp[i - 1][j] == false)
-			if (recursive(board, dp, word.substring(1), i - 1, j))
+		if (i > 0 && board[i - 1][j] != '*')
+			if (recursive(board, word.substring(1), i - 1, j))
 				return true;
-		if (j < board[0].length - 1 && dp[i][j + 1] == false)
-			if (recursive(board, dp, word.substring(1), i, j + 1))
+		if (j < board[0].length - 1 && board[i][j + 1] != '*')
+			if (recursive(board, word.substring(1), i, j + 1))
 				return true;
-		if (i < board.length - 1 && dp[i + 1][j] == false)
-			if (recursive(board, dp, word.substring(1), i + 1, j))
+		if (i < board.length - 1 && board[i + 1][j] != '*')
+			if (recursive(board, word.substring(1), i + 1, j))
 				return true;
-		dp[i][j] = false;
+		board[i][j] = hold;
 		return false;
 	}
 }
